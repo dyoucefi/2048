62fcd1eec49c6c8087f2b18dd912c15b443c3434
Axaxa1 <57354754+Axaxa1@users.noreply.github.com>
2019-11-12 21:51:06 +0100

    Add files via upload

---
+++ pystrat2048-master/pystrat2048/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+"""
+Frequent plotting functions.
+"""
+
+__version__ = "0.1"
+__author__ = "Xavier Dupré"
+
+from .cp2048 import Game2048  # noqa
+from .evaluate import evaluate_strategy  # noqa
